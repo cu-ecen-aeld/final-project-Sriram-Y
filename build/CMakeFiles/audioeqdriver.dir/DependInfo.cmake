64dc061c68f6dd416c965b07aa0560cc51657132
Sriram-Y <sriramy2001@gmail.com>
2024-11-26 07:56:10 +0000

    build directory

---
+++ build/CMakeFiles/audioeqdriver.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/home/dev/final-project/final-project-Sriram-Y/src/audio_processing.c" "/home/dev/final-project/final-project-Sriram-Y/build/CMakeFiles/audioeqdriver.dir/src/audio_processing.c.o"
+  "/home/dev/final-project/final-project-Sriram-Y/src/audioeqdriver.c" "/home/dev/final-project/final-project-Sriram-Y/build/CMakeFiles/audioeqdriver.dir/src/audioeqdriver.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
